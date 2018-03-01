c0a4f17a1cefbafc0239cb5e95a9542817310258
Daniel Champion <16627962@students.lincoln.ac.uk>
2018-03-01 20:50:09 +0000

    gfdfg
    updates

--- code/read.py
+++ code/read.py
@@ -1,3 +1,13 @@
 filename = "inputs/a_example.in"
-text = open(filename,"r")
-lines = sum(1 for line in text)
+file = open(filename,"r")
+lines = sum(1 for line in file)
+count = 0
+out = file.read()
+print(out)
+print(lines)
+
+while(count <= lines - 1):
+    file.readline(count)
+    count += 1
+    
+file.close()
